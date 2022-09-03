b827b95ca0d168a351f21d6a8406020d060c95b6
safeint <zhujian941@gmail.com>
2022-09-03 17:14:20 +0800

    commit
    Signed-off-by: safeint <zhujian941@gmail.com>

---
+++ src/main/java/com/example/demojavafamily/DemoJavaFamilyApplication.java
@@ -0,0 +1,16 @@
+package com.example.demojavafamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhujian
+ */
+@SpringBootApplication
+public class DemoJavaFamilyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJavaFamilyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojavafamily/DemoJavaFamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojavafamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaFamilyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
